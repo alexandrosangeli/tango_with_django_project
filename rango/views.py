86394d0772e06ccaed6792ab5e8b3fd605ce8874
Alexandros Angeli <alexandrosangeli@gmail.com>
2021-01-24 23:07:12 +0200

    done up to 4.2 (serving static files)

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Rango says hey there partner! Go to <a href='/rango/about/'>About</a>")
+    content_dict = {'boldmessage' : 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=content_dict)
 
 def about(request):
     return HttpResponse("Rango says here is the about page. Go back to <a href='/rango/'>Index</a>")
