04daddb1ac13d3aa0748304bd29fa1bd2eeeb8fd
Alexandros Angeli <alexandrosangeli@gmail.com>
2021-01-25 22:59:22 +0200

    added media files - finished ch4 and passed tests

--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.conf import settings
 
 
 def index(request):
-    content_dict = {'boldmessage' : 'Crunchy, creamy, cookie, candy, cupcake!'}
-    return render(request, 'rango/index.html', context=content_dict)
+    context_dict = {'boldmessage' : 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. Go back to <a href='/rango/'>Index</a>")
+    context_dict = {'MEDIA_URL' : settings.MEDIA_URL}
+    return render(request, 'rango/about.html', context=context_dict)
