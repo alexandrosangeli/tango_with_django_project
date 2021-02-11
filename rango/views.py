efb0f43a6a0d6217a5d481c6a7c2d0150a8b696f
Alexandros Angeli <alexandrosangeli@gmail.com>
2021-02-11 23:14:46 +0200

    finished ch10 (cookies & session)

--- rango/views.py
+++ rango/views.py
@@ -7,6 +7,28 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
+from datetime import datetime
+
+def get_server_side_cookie(request, cookie, default_val=0):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, "visits","1"))
+
+    last_visit_cookie = get_server_side_cookie(request, "last_visit", str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+    
+    request.session["visits"] = visits
 
 
 def index(request):
@@ -22,11 +44,16 @@ def index(request):
     pages = Page.objects.order_by('-views')[:5]
     context_dict['pages'] = pages
 
-    return render(request, 'rango/index.html', context=context_dict)
+    visitor_cookie_handler(request)
+
+    response = render(request, 'rango/index.html', context=context_dict)
+    return response
 
 
 def about(request):
-    context_dict = {'MEDIA_URL' : settings.MEDIA_URL}
+    visitor_cookie_handler(request)
+    visits = request.session['visits']
+    context_dict = {'MEDIA_URL' : settings.MEDIA_URL, 'visits':visits}
     return render(request, 'rango/about.html', context=context_dict)
 
 
